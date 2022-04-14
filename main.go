74dd19ab99ffa176d03b07ef549ebff509422c62
Raghavi Shirur <raghaviss11@gmail.com>
2022-04-14 11:22:00 +0530

    test13

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 func main() {
 	script.Exec("kubectl create namespace argocd").Stdout()
-	script.Exec("kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/core-install.yaml").Stdout()
-	// script.Exec("kubectl port-forward svc/argocd-server -n argocd 8080:443").Stdout()
+	script.Exec("kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml").Stdout()
+	script.Exec("kubectl port-forward svc/argocd-server -n argocd 8080:443").Stdout()
 	// script.Exec("kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo").Stdout()
 
 }
