a80101e3196eca8239273d334e2f82fc0dcc9717
Raghavi Shirur <raghaviss11@gmail.com>
2022-04-21 16:36:38 +0530

    file structure change

--- go.mod
+++
@@ -1,7 +0,0 @@
-module main
-
-go 1.17
-
-require github.com/bitfield/script v0.19.0
-
-require bitbucket.org/creachadair/shell v0.0.7 // indirect
---
+++ util/main.py
@@ -0,0 +1,31 @@
+# This script's purpose is to generate a hierarchical file structure as follows : 
+# Each of these dir 1a, 1b, 1c, 1d, 1e, 1f, 1g, 1h, 1i, 1j contain a kustomization.yaml and a dep.yaml
+# Each of these dir 2a . . . 2j contain 1a . . . 1j 
+#                 layer5
+#        /  /  /   / | \  \  \  \  \    
+#       4a 4b 4c 4d 4e 4f 4g 4h 4i 4j
+#        .  .  .  .  .  .  .  .  .  . 
+#        .  .  .  .  .  .  .  .  .  . 
+#        .  .  .  .  .  .  .  .  .  . 
+#       1a-1j.......................1a-1j
+
+import os
+
+def create_yaml(path, resource):
+    yaml = f"apiVersion: kustomize.config.k8s.io/v1beta1\nkind: Kustomization\nnamePrefix: {path.split('/')[-1].replace('layer', '')}-\n\nresources: \n" + resource
+    open(f"{path}/kustomization.yaml", "w").write(yaml)
+
+def burst(path: str, n: int):
+    if n == 0:
+        os.system(f"cp manifests/dep.yaml {path}")
+        create_yaml(path, "- manifests/dep.yaml")
+        return
+
+    for j in "abcdefghij":
+        os.makedirs(f"{path}/layer{n}{j}")
+        burst(f"{path}/layer{n}{j}", n-1)
+
+    create_yaml(path, "\n".join([f"- layer{n}{i}" for i in "abcdefghij"]))
+
+os.mkdir("layer5")
+burst("layer5", 4)
