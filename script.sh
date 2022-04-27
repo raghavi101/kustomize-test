4aac2ffaf48232b1fca3cd46ba7e4faef58299f3
Raghavi Shirur <raghaviss11@gmail.com>
2022-04-27 13:35:18 +0530

    adding set+e to script2

--- script.sh
+++ script.sh
@@ -24,11 +24,13 @@ kubectl apply -n kube-system -f manifests/components.yaml
 
 tput setaf 3; echo "Waiting for ArgoCD to start . . ." ; tput sgr0
 
+set +e
 Resources=("argocd-application-controller" "argocd-applicationset-controller" "argocd-dex-server" "argocd-notifications-controller" "argocd-redis" "argocd-repo-server" "argocd-server")
 for resource in ${Resources[*]}; do
     kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=$resource -n argocd
 done
 
+set -e
 tput setaf 2; echo "Creating application . . ." ; tput sgr0
 
 kubectl apply -n argocd -f ./application.yaml
